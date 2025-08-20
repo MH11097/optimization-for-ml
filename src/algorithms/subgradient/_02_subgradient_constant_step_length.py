523d0c693dcd855dc0d57f30fce9227453c39698
toandm <toandoanftu@gmail.com>
2025-08-20 17:17:41 +0700

    update constant step lenght

--- src/algorithms/subgradient/_02_subgradient_constant_step_length.py
+++ src/algorithms/subgradient/_02_subgradient_constant_step_length.py
@@ -1,1 +1,11 @@
-from ._01_subgradient_constant_step_size import BoToiUuHoaSubgradient
+from ._01_subgradient_constant_step_size import SubgradientConstantStepSize
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class SubgradientConstantStepLength(SubgradientConstantStepSize):
+
+    def get_step_size(self, current_subgradient_vector):
+        subgrad_norm = np.linalg.norm(x=current_subgradient_vector)
+        FIXED_STEP_LENGTH = 0.05
+        return FIXED_STEP_LENGTH / subgrad_norm

c95acd650c749b51115af5ca8998908dad103384
toandm <toandoanftu@gmail.com>
2025-08-26 16:11:16 +0700

    remove one setup

--- src/algorithms/subgradient/__init__.py
+++ src/algorithms/subgradient/__init__.py
@@ -4,6 +4,3 @@ from ._03_subgradient_square_summable import SubgradientSquareSummable
 from ._04_subgradient_non_summable_diminishing_step_size import (
     SubgradientNonSummableDiminishingStepSize,
 )
-from ._05_subgradient_non_summable_diminishing_step_length import (
-    SubgradientNonSummableDiminishingStepLength,
-)
